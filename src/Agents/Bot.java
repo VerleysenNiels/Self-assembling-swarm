fa72243ca543dde80b78f3b14079ce8f0181af2c
Niels Verleysen <nlsverleysen99@gmail.com>
2020-04-22 15:31:38 +0200

    Add gradient formation to the agent

--- src/Agents/Bot.java
+++ src/Agents/Bot.java
@@ -12,18 +12,30 @@ package Agents;
 
 import Environment.Environment;
 import sim.engine.*;
+import sim.util.Bag;
+import sim.util.Double2D;
 
 public class Bot implements Steppable{
     private static final long serialVersionUID = 1;
     
     private Boolean seed;
     private Boolean joined_shape = false;
+    private int gradient;
+    private int MAXGRAD;
+    private double VISIBLE_DIST;
     
     // Agent behaviour
     @Override
     public void step(SimState state) {
         // GET ENVIRONMENT STATE
         Environment env = (Environment)state;
+        
+        Double2D position = env.field.getObjectLocationAsDouble2D(this);
+        Bag neighbors = env.field.getNeighborsWithinDistance(position, this.VISIBLE_DIST, false);
+        
+        if(!this.joined_shape){
+            this.gradient = this.gradient_formation(neighbors);
+        }
     }
     
     // USED ALGORITHMS
@@ -33,15 +45,19 @@ public class Bot implements Steppable{
     }
     
     // Gradient formation
-    private int gradient_formation() {
+    private int gradient_formation(Bag neighbors) {
         if(seed){
           return 0;  
         }
         else{
-            // For all neighbours with a lower distance than a given distance
-            // Get smallest gradient
-            // Own gradient = smallest gradient + 1
-            return 0;
+            int new_gradient = this.MAXGRAD; 
+            for(Object n : neighbors){
+                Bot neighbor = (Bot) n;
+                if(new_gradient < neighbor.getGradient()){
+                    new_gradient = neighbor.getGradient();
+                }
+            }
+            return new_gradient+1;
         }
     }
     
@@ -54,4 +70,15 @@ public class Bot implements Steppable{
     private void check_id() {
         
     }
+    
+    // Getters
+
+    public Boolean getJoined_shape() {
+        return joined_shape;
+    }
+
+    public int getGradient() {
+        return gradient;
+    }
+    
 }
